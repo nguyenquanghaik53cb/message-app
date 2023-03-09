79ca1a7304d3ec1c2a526949a9f1e10dfa1e238d
nguyenquanghaik53cb <nguyenquanghaik53cb@gmail.com>
2023-03-09 23:26:33 +0700

    update migrate files and readme

---
+++ database/migrations/2023_09_03_0100000_create_rooms_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->tinyInteger('status');//0: in-active, 1: active
+            $table->timestamps();//created, updated fields
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
---
+++ database/migrations/2023_09_03_020000_create_room_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('room_users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('room_id');
+            $table->integer('user_id');
+            $table->string('message');//message is sending on chatting two users
+            $table->timestamps();//created, updated fields
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('room_users');
+    }
+};
